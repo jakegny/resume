fa2fd5ec78689ec2024c5d17bc0b2b572cf23d7e
Jake Nylund <jakegny@gmail.com>
2017-05-06 07:42:34 -0700

    styling changes

--- src/views/About/component.js
+++ src/views/About/component.js
@@ -10,10 +10,10 @@ const About = props => {
         I love attacking a problem I haven't come across before.
         I am always learning something new.
         A new language, a new tool, doesn't matter.
-        I'm not truely happy unless I can learn something new everyday.
+        I'm not truly happy unless I can learn something new everyday.
 
         I care about the work I do and the impact it has.
-        It matters a great deal to me. But almost as imporant is that the work is fun and the people I work with can laugh throughout the day.
+        It matters a great deal to me. But almost as important is that the work is fun and the people I work with can laugh throughout the day.
         {' '}
 
       </p>
